db255a8dceaf554839c76d872379a70b7e551d0f
Wesley Bernardes <bw3sley@gmail.com>
2022-06-28 21:31:55 -0300

    :tada: chore: initial commit

---
+++ nlw-setup/react-native/src/components/Header.tsx
@@ -0,0 +1,35 @@
+import { View, TouchableOpacity, Text } from "react-native";
+
+import colors from "tailwindcss/colors";
+
+import { Feather } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+
+import { useNavigation } from "@react-navigation/native";
+
+export function Header() {
+    const { navigate } = useNavigation();
+
+    return (
+        <View className="w-full flex-row items-center justify-between">
+            <Logo />
+
+            <TouchableOpacity
+                activeOpacity={.7}
+                className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+                onPress={() => navigate("new")}
+            >
+                <Feather 
+                    name="plus"
+                    color={colors.violet[500]}
+                    size={20}
+                />
+
+                <Text className="text-white ml-3 font-semibold text-base">
+                    Novo
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
