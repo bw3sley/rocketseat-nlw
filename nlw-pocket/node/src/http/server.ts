5766820451f24cc55467d7a0164cb74fdb036d49
Wesley Bernardes <bw3sley@gmail.com>
2024-09-16 14:56:23 -0300

    :tada: chore: initial commit

---
+++ nlw-pocket/node/src/http/server.ts
@@ -0,0 +1,31 @@
+import fastify from "fastify";
+
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+
+import { env } from "../env";
+
+import { createGoalRoute } from "./routes/create-goal";
+import { createCompletionRoute } from "./routes/create-completion";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
+
+import fastifyCors from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: "*",
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app.listen({
+  port: env.PORT,
+  host: "0.0.0.0"
+}).then(() => console.log(`🔥 HTTP server running at localhost:${env.PORT}`));
